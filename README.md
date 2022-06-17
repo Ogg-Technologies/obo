b0f24ea63c7ca98528b3d5dd015cc0373b232769
Oskar <ogg.developer@gmail.com>
2022-06-17 19:17:23 +0200

    Readme changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Ogg's Bee Optimizer
+
+1. Edit `config.lua` with the traits you want the target bee to have
+2. Place an analyzed princess and drones in the chest OR start the apiary with a princess and drone
+3. Execute the command `run`
--- config.lua
+++ config.lua
@@ -5,10 +5,12 @@ return {
         effect = {trait = "None", importance = 1},
         fertility = {trait = 4.0, importance = 1},
         flowerProvider = {trait = "Flowers", importance = 1},
+        humidityTolerance = {trait = "Both 5", importance = 1},
         lifespan = {trait = 10.0, importance = 1},
         nocturnal = {trait = true, importance = 1},
-        species = {trait = "Unweary", importance = 1},
-        speed = {trait = 1.2000000476837, importance = 1},
+        species = {trait = "Fertilizer", importance = 1},
+        speed = {trait = 1.7000000476837, importance = 1},
+        temperatureTolerance = {trait = "Both 5", importance = 1},
         tolerantFlyer = {trait = true, importance = 1}
     },
     runSimulations = true
